08d75c0779cfe481fccaedfea7fd942b499c0b82
RAFA ATH ZANAR <zanarmhmra.21@uom.lk>
2025-09-29 15:20:00 +0530

    feat: update Hero component text for improved clarity and focus on microservices experience

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -117,19 +117,15 @@ const Hero = () => {
             ,
             <span className="px-2 py-0.5 mx-1 rounded text-white bg-gradient-to-r from-red-700 via-orange-800 to-yellow-900 bg-200% animate-gradient-x">
               React Native
-            </span>
-            , and
-            <span className="px-2 py-0.5 mx-1 rounded text-white bg-gradient-to-r from-lime-700 via-green-800 to-emerald-900 bg-200% animate-gradient-x">
-              Angular
-            </span>
-            with microservices, I enjoy building scalable, reliable, and
-            user-focused applications.
+            </span>{" "}
           </p>
-
           <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
-            I have experience with production-grade systems, CI/CD pipelines,
-            and observability tools, which strengthened my ability to deliver
-            practical, high-impact solutions.
+            I have experience in working with microservices, production-grade
+            systems, CI/CD pipelines, and observability tools, which
+            strengthened my ability to deliver practical, high-impact solutions.
+          </p>
+          <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
+            I enjoy building scalable, reliable, and user-focused applications
           </p>
         </div>
       </div>
